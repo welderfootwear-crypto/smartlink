06094b84198cdf28c0e5984748feb3fc145d9aab
welderfootwear-crypto <welderfootwear@gmail.com>
2026-02-23 21:19:44 +0700

    Create redirect.ts

---
+++ api/api/redirect.ts
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "No URL provided" });
+  }
+
+  return res.redirect(url);
+}
